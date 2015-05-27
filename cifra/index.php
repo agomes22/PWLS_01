1dd00e1506c460fc1b2dbf80b5839cdf8a969305
31243 <31243@ufp.edu.pt>
2015-05-27 10:14:36 +0100

    chave em ficheiro

--- cifra/index.php
+++ cifra/index.php
@@ -6,20 +6,20 @@
     <meta charset="UTF-8">
     <title>Trabalho Segurança</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-
 </head>
 
 <body>
     <div id="wrapper">
         <form enctype="multipart/form-data" action="index.php" method="post">
             <fieldset>
-                <legend>Cifrar E Decifrar</legend>
+                <legend>Cifrar E Decifrar Vigenère</legend>
                  <div>
                     <textarea name="texto" placeholder="Texto a cifrar ou insira um ficheiro .txt abaixo"></textarea>
                     <input type="file" name="ficheiro" accept="txt"/>
                 </div>  
                 <div>
-                    <input type="text" name="chave" placeholder="Chave da Cifra" required/>
+                    <input type="text" name="chave" placeholder="Chave da Cifra ou insira um ficheiro .txt abaixo"/>
+                    <input type="file" name="ficheiroChave" accept="txt"/>
                 </div>  
                 <div class="small"><input type="radio" name="tipo" value="cifrar" checked>Cifrar</div>
                 <div class="small"> <input type="radio" name="tipo" value="decifrar">Decifrar</div>
@@ -29,24 +29,62 @@
 <?php 
 if (isset($_POST['submite']))
 {   
-    $chave=$_POST['chave'];//chave do formulario
+    
     $tipo=$_POST['tipo'];//tipo do formulario
         
-    //COLOCAR EM MAISCULAS
-    $chave=strtoupper($chave);
     
     //VARIAVEIS
-    $tamChave=strlen($chave)-1;
     $controlador=0;
     $textoCifrado=" ";
     $textoChave=" ";
+    if($_FILES['ficheiroChave']['name'] != "") 
+    {
     
+        //VERIFICA SE O FICHEIRO SO TEM TEXTO
+        if(isset($_FILES) && $_FILES['ficheiroChave']['type'] != 'text/plain')
+        {
+            echo "<b><span>Ficheiro nao pode ser aceite ! Faça o upload de ficheiro com extensao '*.txt'.</span></b>";
+            exit();
+        }            
+ 
+        //ARMAZENA TEMPORARIAMENTE O NOME DO FICHEIRO
+        $fileName = $_FILES['ficheiroChave']['tmp_name'];
+ 
+        //MENSAGEM DE ERRO SE NAO CONSEGUIR ABRIR
+        $ficheir = fopen($fileName,"r") or exit("<b>Nao é possivel abrir!</b>");
+        $linhas = count(file($fileName));
+        $chave=" ";
+        $i=0;
+        if ($linhas==1)
+        {
+           while(!feof($ficheir)) 
+            {
+                $chave[$i]=fgetc($ficheir);
+                $i++;
+            }
+        fclose($ficheir);
+        }
+          //TRATAMENTO VARIAVEL CHAVE se tiver no ficheiro
+        $chave=strtoupper($chave);
+        $tamChave=strlen($chave)-2;
+    }//SE NAO TIVER FICHEIRO VAI BUSCAR A CHAVE AO FORMULARIO
+    else
+    {
+        $chave=$_POST['chave'];//chave do formulario
+          //TRATAMENTO VARIAVEL CHAVE se tiver no formulario
+        $chave=strtoupper($chave);
+        $tamChave=strlen($chave)-1;
+    }
+   
+
     $controlo=1;//variavel e ciclo para ver se a chave tem outros caracteres sem ser letras
+ 
     for ($i=0;$i<=$tamChave;$i++)
     {
         if ($controlo!=0)
         {
-            if ($chave[$i]>=chr(65) and $chave[$i]<=chr(90))
+             echo $chave[$i];
+            if (ord($chave[$i])>=65 and ord($chave[$i])<=90)
             {
                 $controlo=1;
             }
@@ -56,6 +94,7 @@ if (isset($_POST['submite']))
             }
         }
     }
+   echo $controlo;
     if ($controlo==1)
     {
      
@@ -78,7 +117,7 @@ if (isset($_POST['submite']))
   
         $texto=" ";
         $i=0;
-        //COLOCA NA VARIAVEL TEXTO O TEXTO DO FICHEIRO
+        //COLOCA NA VARIAVEL $TEXTO O TEXTO DO FICHEIRO
         while(!feof($ficheir)) 
         {
             $texto[$i]=fgetc($ficheir);
@@ -93,7 +132,6 @@ if (isset($_POST['submite']))
     //TRATAMENTO VARIAVEL TEXTO
     $texto=strtoupper($texto);
     $tamTexto=strlen($texto)-1;
-    
     //CICLO PARA COLOCAR A CHAVE NO TAMANHO DO TEXTO
     for ($i=0;$i<=$tamTexto;$i++)
     {
@@ -172,12 +210,14 @@ if (isset($_POST['submite']))
         fclose($ficheiro);
         echo "<br><a download href='TextoCifrado.txt'><img alt='Download Texto Cifrado' height='60' width='210' src='download.png'></a>" ;
 
-        }
+    }
+}
 else 
 {
     echo "<b>A Chave só pode ter letras</b>";
 }
-}
+
+
 }
 ?>
 
