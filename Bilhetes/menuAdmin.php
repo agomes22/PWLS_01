02b0078991ec6475276adf3af82aa28fc9b5b771
31243 <31243@ufp.edu.pt>
2015-06-05 11:27:51 +0100

    pp

---
+++ Bilhetes/menuAdmin.php
@@ -0,0 +1,9 @@
+<?php
+echo '<div class="menu">
+    <a href="index.php">Home</a>
+    <a href="#2>Bilhetes</a>
+    <a href="#3">Vencedores</a>
+    <a href="#4">Utilizadores</a>
+    <a href="#5">Logout</a>
+</div>'
+?>
